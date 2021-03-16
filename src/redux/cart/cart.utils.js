89968e694374f67a306ddc00a4496c3033a71ddc
G-Likhitha <G.Likhitha@ibm.com>
2021-03-16 14:19:19 +0530

    checkout page adding the items in increment and decrement

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -10,3 +10,21 @@ export const addItemToCart = (cartItems, carttItemToAdd) =>{
     }
     return [...cartItems, {...carttItemToAdd, quantity:1} ];
 }
+export const removeItemFromCart = (cartItems, cartItemToRemove)=>{
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+
+    );
+    if(existingCartItem.quantity == 1){
+        return cartItems.filter(
+            cartItem => cartItem.id === cartItemToRemove.id 
+           
+        )
+    }
+    return cartItems.map(
+        cartItem => cartItem.id ?
+        {...cartItem, quantity : cartItem.quantity -1}
+        : cartItem
+    )
+
+}
